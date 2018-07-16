52050e65212b5facd00cc12893232929bcd6a064
H <hsl1991@live.com>
2018-07-16 11:14:56 +0800

    添加项目文件。

---
+++ aspnetmvclifecycle/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(aspnetmvclifecycle.Startup))]
+namespace aspnetmvclifecycle
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
