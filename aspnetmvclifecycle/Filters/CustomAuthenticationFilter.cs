d208a80969a582cbc64fc5467d5d63bdc95a7244
H <hsl1991@live.com>
2018-07-16 12:25:42 +0800

    1

---
+++ aspnetmvclifecycle/Filters/CustomAuthenticationFilter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc.Filters;
+
+namespace aspnetmvclifecycle.Filters
+{
+    /// <summary>
+    /// 身份认证
+    /// </summary>
+    public class CustomAuthenticationFilter : IAuthenticationFilter
+    {
+        public void OnAuthentication(AuthenticationContext filterContext)
+        {
+            if (filterContext.Result == null)
+            {
+            }
+            //todo:操作http请求上下文
+            var queryStrings = filterContext.RequestContext.HttpContext.Request.QueryString;
+            if (queryStrings != null && queryStrings.Count > 0)
+            {
+                foreach (var queryitem in queryStrings)
+                {
+
+                }
+            }
+            var forms = filterContext.RequestContext.HttpContext.Request.Form;
+            if (forms != null && forms.Count > 0)
+            {
+                foreach (var formitem in forms)
+                {
+
+                }
+            }
+
+        }
+
+        public void OnAuthenticationChallenge(AuthenticationChallengeContext filterContext)
+        {
+            if (filterContext.Result == null)
+            {
+                return;
+            }
+        }
+    }
+}
---
+++ aspnetmvclifecycle/Filters/CustomAuthorizationFilter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace aspnetmvclifecycle.Filters
+{
+    /// <summary>
+    /// 授权
+    /// </summary>
+    public class CustomAuthorizationFilter : IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if (filterContext.Result == null)
+            {
+
+            }
+        }
+    }
+}
