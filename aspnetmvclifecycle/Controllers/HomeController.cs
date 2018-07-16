af0d067510a4fb0d1eeac3d772f0833f9ce9cd5e
H <hsl1991@live.com>
2018-07-16 15:51:54 +0800

    2

--- aspnetmvclifecycle/Controllers/HomeController.cs
+++ aspnetmvclifecycle/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using aspnetmvclifecycle.Filters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,7 @@ namespace aspnetmvclifecycle.Controllers
 {
     public class HomeController : Controller
     {
+        [CustomFilter]
         public ActionResult Index()
         {
             return View();
---
+++ aspnetmvclifecycle/Filters/CustomFilter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace aspnetmvclifecycle.Filters
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = false)]
+    public class CustomFilter : ActionFilterAttribute
+    {
+        public bool EnableFilter { get;private set; }
+
+        public CustomFilter()
+        {
+            EnableFilter = true;
+        }
+
+        public CustomFilter(bool enableFilter)
+        {
+            EnableFilter = enableFilter;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext == null)
+            {
+                throw new ArgumentNullException("Action Executing Context is Null!");
+            }
+
+            string actionName = filterContext.ActionDescriptor.ActionName;
+            string controllerName = filterContext.ActionDescriptor.ControllerDescriptor.ControllerName;
+
+            //todo:自定义功能
+
+            #region 1 自动回传请求参数
+
+            var forms = filterContext.HttpContext.Request.Form;
+            if (forms != null && forms.Count > 0)
+            {
+                foreach (string key in forms.AllKeys)
+                {
+                    filterContext.Controller.ViewData[key] = forms[key];
+                }
+            }
+
+            var queryStrings = filterContext.HttpContext.Request.QueryString;
+            if (queryStrings != null && queryStrings.Count > 0)
+            {
+                foreach (string key in queryStrings.AllKeys)
+                {
+                    filterContext.Controller.ViewData[key] = queryStrings[key];
+                }
+            }
+            #endregion
+
+        }
+    }
+}
